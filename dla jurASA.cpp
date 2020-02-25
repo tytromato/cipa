05ff7d2c02bdd65d8d182973e1952d555823dc2f
tytromato <61473619+tytromato@users.noreply.github.com>
2020-02-25 21:00:47 +0100

    Add files via upload
    EJ.TO JEST TO CZEGO SZUKASZ.BUCU JEDEN EDYTUJ TO A NIE DUO.CPP OK?

---
+++ dla jurASA.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	//juras ogólnie to help bo tu jest w...du¿o bledow 
+int odpowiedz=0;
+cout <<"idziesz do kolegi ,by zagrac w wota (odpowiedz na to pytanie 2) .";
+
+cin>>odpowiedz;
+//if (odpowiedz=2)
+//cout << "uno";
+
+if (odpowiedz=2);
+cout << "docierasz. komputer kolegi jest zepsuty, wiêc 1)idziesz sobie 2)zostajesz bo masz swojego lapka ";
+
+
+int odpowiedz2=0;
+cin >>odpowiedz2;
+if (odpowiedz2=1);
+
+cout<< "wracasz do domu. mamy nie ma,wiec obiadu te¿ nie. co robisz? 1) wracasz do domu kolegi,bo ma obiad. 2) idziesz do siebie bo masz klucz. w lodowce na pewno cos jest";
+
+int odpowiedz3=0;
+cin >>odpowiedz2;
+if (odpowiedz3=2);
+cout << "wyciagasz lapka.rozladowany.na zczescie kolega ma zasilacz. 1)po¿yczasz 2)jesteœ wstydliwym debilem i nie po¿yczasz.";
+
+
+
+
+
+
+
+}  
+
